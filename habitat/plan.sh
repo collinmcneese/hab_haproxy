b5a12a46382a492747e988dfb78909836fb83156
Collin McNeese <cmcneese@chef.io>
2020-08-15 01:17:03 -0500

    pkg_source correction
    Signed-off-by: Collin McNeese <cmcneese@chef.io>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -4,7 +4,7 @@ pkg_maintainer="Collin McNeese <cmcneese@chef.io>"
 pkg_version=2.2.2 # Bump version to match version of haproxy to use and reference in pkg_source
 # From upstream core/haproxy
 pkg_license=('GPL-2.0' 'LGPL-2.1')
-pkg_source="https://www.haproxy.org/download/2.1/src/haproxy-${pkg_version}.tar.gz"
+pkg_source="https://www.haproxy.org/download/2.2/src/haproxy-${pkg_version}.tar.gz"
 pkg_upstream_url="https://www.haproxy.org/"
 pkg_shasum=391c705a46c6208a63a67ea842c6600146ca24618531570c89c7915b0c6a54d6
 #pkg_svc_run=''
